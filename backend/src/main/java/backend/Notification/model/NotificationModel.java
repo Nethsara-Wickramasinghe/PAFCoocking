98aaf40faf75ed002c5c4d221b44da91a9ea8768
hiruna dilhara <ffhiru@gmail.com>
2025-05-21 14:21:46 +0530

    Refactor code structure for improved readability and maintainability

--- backend/src/main/java/backend/Notification/model/NotificationModel.java
+++ backend/src/main/java/backend/Notification/model/NotificationModel.java
@@ -14,7 +14,7 @@ public class NotificationModel {
     private boolean read; // Whether the notification has been read
     private String createdAt; // Timestamp of the notification
 
-    
+
     public NotificationModel(){}
 
     public NotificationModel(String userId, String message, boolean read, String createdAt) {
